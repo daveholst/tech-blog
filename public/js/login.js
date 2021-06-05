a49b804d11925de373ade5582e8360d27df8a641
Dave Holst <daveholst@gmail.com>
2021-06-05 12:18:13 +0800

    API: logout working from browser

--- public/js/login.js
+++ public/js/login.js
@@ -9,17 +9,31 @@ const loginBackground = document.querySelector('#login-bg')
 const signupModal = document.querySelector('#signup-modal');
 const signupCloseBtn = document.querySelector('#signup-close-btn');
 const signupBackground = document.querySelector('#signup-bg')
-
+// Hamburger
+const navMenu = document.querySelector('#nav-menu');
+const navBurger = document.querySelector('#burger');
 // * Modal handlers
 // open when login from nav is clicked
-loginButton.addEventListener('click', () => {
-  loginModal.classList.toggle('is-active');
-})
-// open when signup from nav is clicked
-signupButton.addEventListener('click', () => {
-  signupModal.classList.toggle('is-active');
+loginButton.addEventListener('click', async () => {
+  if (loginButton.textContent.includes('Log In')) {
+    loginModal.classList.toggle('is-active');
+  } else {
+    try {
+      const response = await axios.post('/api/users/logout')
+      window.location.replace("/");
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 })
 
+// open when signup from nav is clicked
+if (signupButton) {
+  signupButton.addEventListener('click', () => {
+    signupModal.classList.toggle('is-active');
+  })
+}
 // close when login modal-background is clicked
 loginBackground.addEventListener('click', () => {
   loginModal.classList.toggle('is-active');
@@ -39,3 +53,29 @@ signupCloseBtn.addEventListener('click', () => {
   signupModal.classList.toggle('is-active');
 })
 
+// !open hamburger -- copypasta from bulma.io docs
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+});
+// ! end copypasta
